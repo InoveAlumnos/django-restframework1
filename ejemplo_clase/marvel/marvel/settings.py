bc092712ac2f0a369a94851d35d27fe3aa86427b
inoveAlumnos <alumnos@inove.com.ar>
2023-10-24 20:44:53 -0300

    Agregar variable de entorno DB_ENGINE

--- ejemplo_clase/marvel/marvel/settings.py
+++ ejemplo_clase/marvel/marvel/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,23 +106,33 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# NOTE: Reemplazamos la configuración inicial de base de datos para trabajar con Postgres:
-# Recordemos:
-    #   POSTGRES_DB: marvel_db
-    #   POSTGRES_USER: inove_user
-    #   POSTGRES_PASSWORD: 123Marvel!
+if os.getenv("DB_ENGINE") == "POSTGRES":
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2' --> En desuso.
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'marvel_db',        # POSTGRES_DB
-        'USER' : 'inove_user',      # POSTGRES_USER
-        'PASSWORD' : '123Marvel!',  # POSTGRES_PASSWORD
-        'HOST':'db',                # Nombre del servicio
-        'PORT': '5432'              # Número del puerto
+    # NOTE: Reemplazamos la configuración inicial de base de datos para trabajar con Postgres:
+    # Recordemos:
+        #   POSTGRES_DB: marvel_db
+        #   POSTGRES_USER: inove_user
+        #   POSTGRES_PASSWORD: 123Marvel!
+
+    DATABASES = {
+        'default': {
+            # 'ENGINE': 'django.db.backends.postgresql_psycopg2' --> En desuso.
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'marvel_db',        # POSTGRES_DB
+            'USER' : 'inove_user',      # POSTGRES_USER
+            'PASSWORD' : '123Marvel!',  # POSTGRES_PASSWORD
+            'HOST':'db',                # Nombre del servicio
+            'PORT': '5432'              # Número del puerto
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

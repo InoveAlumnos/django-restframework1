69161d3a18791960ff6052dc94d16921c95ba3ac
inoveAlumnos <alumnos@inove.com.ar>
2024-08-17 10:44:29 -0300

    Agregar names a las urls de ejemplo ed clase

--- ejemplo_clase/marvel/e_commerce/api/urls.py
+++ ejemplo_clase/marvel/e_commerce/api/urls.py
@@ -3,7 +3,7 @@ from e_commerce.api.views import *
 
 
 urlpatterns = [
-    path('comic-list/', comic_list_api_view),
-    path('comic-retrieve/', comic_retrieve_api_view),
-    path('comic-create/', comic_create_api_view)
+    path('comic-list/', comic_list_api_view, name="comic_list_api_view"),
+    path('comic-retrieve/', comic_retrieve_api_view, name="comic_retrieve_api_view"),
+    path('comic-create/', comic_create_api_view, name="comic_create_api_view"),
 ]

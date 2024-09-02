ee5dd11a87fad8b06351118fd625edf1961cfcce
inoveAlumnos <alumnos@inove.com.ar>
2024-09-02 14:06:33 -0300

    Actualizar tests sin API marvel

--- ejercicios_practica/marvel/pytest_fixtures.py
+++ ejercicios_practica/marvel/pytest_fixtures.py
@@ -1,5 +1,7 @@
 import pytest
 
+from e_commerce.models import Comic
+
 
 @pytest.fixture
 def create_user(django_user_model):
@@ -10,3 +12,32 @@ def create_user(django_user_model):
             email='root@gmail.com'
         )
     return make_user
+
+
+@pytest.fixture
+def create_comic():
+    def make_comic(**kwargs):
+        return Comic.objects.create(
+            marvel_id=1010,
+            title='Inove SuperHeroe',
+            description='Coding School',
+            price=10.0,
+            stock_qty=15,
+            picture=''
+        )
+    return make_comic
+
+@pytest.fixture
+def create_list_of_comic():
+    def make_comics(**kwargs):
+        for i in range(10):
+            Comic.objects.create(
+                marvel_id=(1000+i) if i % 2 else (100+i),
+                title=f'Inove SuperHeroe {i+1}',
+                description='Coding School',
+                price=i,
+                stock_qty=5 if i % 2 else 3,
+                picture=''
+            )
+    return make_comics
+
